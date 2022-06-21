b6ada605951ce54c4c15d8542b9e9f2027658d98
YJ You <86042155+DEVIt-Jay-You@users.noreply.github.com>
2022-06-21 19:17:59 +1200

    Initial API setup

---
+++ backend/index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+dotenv.config()
+
+// Connect DB
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB is connected'))
+  .catch((err) => console.log('error: ', err))
+
+// Route
+app.listen(4000, () => {
+  console.log('Server running on port 4000')
+})
