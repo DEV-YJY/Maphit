0bbef39d2e02ee4ae0528c1131410232a224a8e3
YJ You <86042155+DEVIt-Jay-You@users.noreply.github.com>
2022-06-24 23:54:53 +1200

    Render Google map

--- frontend/src/components/Map.jsx
+++ frontend/src/components/Map.jsx
@@ -1,16 +1,51 @@
 import React from 'react'
-import { useParams } from 'react-router-dom'
+import {
+  useLoadScript,
+  LoadScript,
+  GoogleMap,
+  useJsApiLoader,
+} from '@react-google-maps/api'
 
-function Map() {
-
-  googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
-  const params = useParams()
-
-
-
-  return (
-    <div>Map</div>
-  )
+const containerStyle = {
+  width: '400px',
+  height: '400px',
 }
 
-export default Map
+const center = {
+  lat: -3.745,
+  lng: -38.523,
+}
+
+export default function Map() {
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: 'AIzaSyAPBNI3ndJDJk0KwEXWI35mWYMKkB09G0A',
+  })
+
+  const [map, setMap] = React.useState(null)
+
+  const onLoad = React.useCallback(function callback(map) {
+    const bounds = new window.google.maps.LatLngBounds(center)
+    map.fitBounds(bounds)
+    setMap(map)
+  }, [])
+
+  const onUnmount = React.useCallback(function callback(map) {
+    setMap(null)
+  }, [])
+
+  return isLoaded ? (
+    <GoogleMap
+      mapContainerStyle={containerStyle}
+      center={center}
+      zoom={10}
+      onLoad={onLoad}
+      onUnmount={onUnmount}
+    >
+      {/* Child components, such as markers, info windows, etc. */}
+      <></>
+    </GoogleMap>
+  ) : (
+    <></>
+  )
+}
