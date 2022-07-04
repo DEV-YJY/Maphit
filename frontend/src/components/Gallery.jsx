73f2b67f1d86801a359ea395fa9ad367dca14a08
YJ You <86042155+DEVIt-Jay-You@users.noreply.github.com>
2022-07-04 19:11:25 +1200

    Start adding styling to Gallery component

--- frontend/src/components/Gallery.jsx
+++ frontend/src/components/Gallery.jsx
@@ -18,7 +18,7 @@ function Gallery() {
     // console.log(state.album)
     return state.album.albumList
   })
-  // console.log('albumList:', albumList)
+  console.log('albumList:', albumList)
 
   return (
     <>
@@ -32,12 +32,20 @@ function Gallery() {
         {albumList !== [] &&
           albumList.map((album) => {
             return (
-              <div key={album._id}>
-                <div>Place of Visit: {album.place.placeName}</div>
+              <div className='grid grid-cols-3 gap-1' key={album._id}>
                 <div>Album name: {album.name}</div>
                 <div>
                   <Link to={`/upload/${album._id}`}>View Album</Link> (
                   {album.images.length}) photos
+                </div>
+                <div className='relative'>
+                  <div className='absolute inset-0 z-10 flex transition duration-200 ease-in hover:opacity-0'>
+                    <div className='absolute inset-0 bg-black opacity-70'></div>
+                    <div className='mx-auto text-white z-10 self-center uppercase tracking-widest text-sm'>
+                      Place of Visit: {album.place.placeName}
+                    </div>
+                  </div>
+                  <img src={`http://localhost:4000/${album.images[0]}`} />
                 </div>
                 <div>---------------------------------------</div>
               </div>
