a139aa186418ab33a0967587ea2e1bf4e0d5c0b1
YJ You <86042155+DEVIt-Jay-You@users.noreply.github.com>
2022-07-22 16:17:54 +1200

    Complete first nav test

--- frontend/src/__tests__/Footer.test.jsx
+++ frontend/src/__tests__/Footer.test.jsx
@@ -6,6 +6,7 @@ import Footer from '../components/Footer'
 describe('<Footer />', () => {
   it('renders <Footer /> component correctly', () => {
     render(<Footer />)
+    expect.assertions(1)
     const footer = screen.getByRole('heading')
     expect(footer.innerHTML).toContain('Copyright')
   })
---
+++ frontend/src/__tests__/Nav.test.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { screen, render } from '@testing-library/react'
+import { Router } from 'react-router-dom'
+import { createMemoryHistory } from 'history'
+
+import Nav from '../components/Nav'
+
+describe('<Nav />', () => {
+  it('renders <Nav /> component correctly', () => {
+    const history = createMemoryHistory({ initialEntries: ['/albums'] })
+
+    render(
+      <Router location={history.location} navigator={history}>
+        <Nav />
+      </Router>
+    )
+    const nav = screen.getAllByRole('img')
+    console.log(nav[0])
+    expect(nav[0].alt).toContain('left-arrow')
+  })
+})
