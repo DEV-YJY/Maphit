4deae13ae27a1f45340e6de34486d3be1940617a
YJ You <86042155+DEVIt-Jay-You@users.noreply.github.com>
2022-07-25 14:28:44 +1200

    Create initial user controller for auth

---
+++ backend/controllers/userController.js
@@ -0,0 +1,14 @@
+// login user
+const loginUser = async (req, res) => {
+  res.json({ msg: 'login user' })
+}
+
+// signup user
+const signupUser = async (req, res) => {
+  res.json({ msg: 'signup user' })
+}
+
+module.exports = {
+  loginUser,
+  signupUser,
+}
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -3,3 +3,11 @@
 const express = required('express')
 
 const router = express.Router()
+
+// login route
+router.post('/login', (req, res) => {})
+
+// sign-up route
+router.post('/signup', (req, res) => {})
+
+module.exports = router
