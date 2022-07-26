0b0009df61ac65c80e6325a3f656734e21563a36
YJ You <86042155+DEVIt-Jay-You@users.noreply.github.com>
2022-07-26 16:56:33 +1200

    Generate token

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,11 @@
 const User = require('../models/userModel')
+const jwt = require('jsonwebtoken')
+
+const createToken = (_id) => {
+  return jwt.sign({ _id }, process.env.SECRET, { expiresIn: '3d' })
+}
+
+console.log(createToken())
 
 // login user
 const loginUser = async (req, res) => {
@@ -11,7 +18,10 @@ const signupUser = async (req, res) => {
 
   try {
     const user = await User.signup(name, email, password)
-    res.status(200).json({ name, email, user })
+
+    // create a token
+    const token = createToken(user._id)
+    res.status(200).json({ name, email, token })
   } catch (error) {
     res.status(400).json({ error: error.message })
   }
