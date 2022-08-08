7276cf1efc1ecde951c7b915fcec39a3fa431755
YJ You <86042155+DEVIt-Jay-You@users.noreply.github.com>
2022-08-09 08:56:28 +1200

    Login redirects to protect routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,13 +5,16 @@ import Album from './components/Album'
 import Map from './components/Map'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import Nav from './components/Nav'
 import Footer from './components/Footer'
 import Login from './components/Login'
 import Signup from './components/Signup'
+import { useAuthContext } from './hook/useAuthContext'
 
 function App() {
+  const { user } = useAuthContext()
+
   return (
     <div className='font-margarine text-black bg-gray-100 dark:text-darkModeTxt dark:bg-darkModeBg transition duration-700'>
       {/* <Nav /> */}
@@ -19,9 +22,9 @@ function App() {
       <div>
         {/* // isAuthenticated ? render below otherwise : render /auth */}
         <Routes>
-          <Route path='/login' element={<Login />} />
-          <Route path='/signup' element={<Signup />} />
-          <Route path='/' element={<Gallery />} />
+          <Route path='/login' element={!user ? <Login /> : <Navigate to='/' />} />
+          <Route path='/signup' element={!user ? <Signup /> : <Navigate to='/' />} />
+          <Route path='/' element={user ? <Gallery /> : <Navigate to='/login' />} />
           <Route path='/add' element={<AddAlbum />} />
           <Route path='/upload/:albumId' element={<Album />} />
           <Route path='/upload/:albumId/map' element={<Map />} />
